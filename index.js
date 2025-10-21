bac69797deabe751b66c65da943e6b924df595da
Sung Kim <sungkim@student.midlandstech.edu>
2025-10-21 17:42:59 -0400

    Fix bug #2: Invalid comparison operator

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function checkGuess() {
     numberOfGuessesMessage.innerHTML = `You guessed ${guess}. <br> ${remainingAttempts} guesses remaining`;
   }
 
-  if (attempts ==== maxNumberOfAttempts) {
+  if (attempts === maxNumberOfAttempts) {
     submitButton.disabled = true;
     guessInput.disabled = true;
   }
